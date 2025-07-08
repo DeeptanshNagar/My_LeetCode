dbdbcb316fbf8c460742a2d095065a10ce1b2330
Diptansh <diptanshnagar@gmail.com>
2025-07-08 20:38:06 +0530

    Time: 1 ms (99.91%), Space: 43.8 MB (29.74%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < arr.length; i++) { 
+            start = 0;
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                start = end + 1;
+            }
+            end = outer.size() - 1;
+            for (int j = start; j <= end; j++) {
+                List<Integer> internal = new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
+}
