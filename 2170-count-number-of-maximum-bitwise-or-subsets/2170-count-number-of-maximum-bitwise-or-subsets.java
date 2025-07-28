abf1909f266575ef8477dfe385ce84aa9b40c973
Diptansh <diptanshnagar@gmail.com>
2025-07-29 01:09:01 +0530

    Time: 8 ms (80.39%), Space: 41.2 MB (54.96%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
@@ -1,25 +1,14 @@
 class Solution {
-    HashMap<Integer, Integer> map = new HashMap<>();
-    ArrayList<Integer> list = new ArrayList<>();
-    private void helper(int arr[], int i) {
-        if (i == arr.length) {
-            if (list.size() == 0) return;
-            int temp = list.get(0);
-            for (int num : list) temp |= num;
-            map.put(temp, map.getOrDefault(temp, 0) + 1);
-            return;
-        }
-        helper(arr, i + 1);
-        list.add(arr[i]);
-        helper(arr, i + 1);
-        list.removeLast();
-    }
-
     public int countMaxOrSubsets(int[] nums) {
-        helper(nums, 0);
-        int max = 0;
-        for (int num : map.keySet()) max = Math.max(max, num);
-        return map.get(max);
+        int maxOr = 0;
+        for (int n : nums) maxOr |= n;
+        return dfs(nums, 0, 0, maxOr);
     }
 
+    int dfs(int[] nums, int index, int currOr, int maxOr) {
+        if (index == nums.length) return currOr == maxOr ? 1 : 0;
+        int include = dfs(nums, index + 1, currOr | nums[index], maxOr);
+        int exclude = dfs(nums, index + 1, currOr, maxOr);
+        return include + exclude;
+    }
 }
