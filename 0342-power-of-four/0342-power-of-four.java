e283753c43864b1097df0eb2ce722547bbd23ce9
Diptansh <diptanshnagar@gmail.com>
2025-08-16 23:54:07 +0530

    Time: 0 ms (100%), Space: 41.3 MB (15.83%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,7 +1,14 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if (n <= 0) return false;
-        if ((n & (n - 1)) == 0 && Integer.bitCount(n - 1) % 2 == 0) return true;
+       while(n>1){
+        if(n%4 != 0)
+            return false;
+         else
+             n/=4;
+        }
+        if(n==1)
+        return true;
+        else
         return false;
-    }
+     }
 }
