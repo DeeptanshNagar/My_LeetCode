7c4c51275746ac35f1ce81fbad7b7285f1484a10
Diptansh <diptanshnagar@gmail.com>
2025-09-07 20:11:27 +0530

    Time: 0 ms (100%), Space: 41.5 MB (86.14%) - LeetHub

---
+++ 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] sumZero(int n) {
+        int[] ans = new int[n];
+        int i = 0;
+
+        if ((n & 1) == 1){
+            ans[i++] = 0;
+        }
+
+        int k = 1;
+
+        while (i < n) {
+            ans[i] = -k;
+            i++;
+            ans[i] = k;
+            i++;
+            k++;
+        } 
+
+        return ans;
+    }
+}
