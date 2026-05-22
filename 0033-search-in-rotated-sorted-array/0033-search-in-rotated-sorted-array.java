bb76ce1f7dfc95c1504e3a9a07a6b4d47828271d
Diptansh <diptanshnagar@gmail.com>
2026-05-22 16:09:49 +0530

    Time: 0 ms (100%), Space: 44 MB (26.61%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,54 +1,27 @@
 class Solution {
-    int search(int[] nums, int target) {
-        int pivot = findPivot(nums);
-        // If no pivot is found, the array is not rotated
-        if (pivot == -1) {
-            // just do normal binary search
-            return binarySearch(nums, target, 0, nums.length - 1);
-        }
-        // If pivot is found, we have found two ascending arrays
-        if (nums[pivot] == target) {
-            return pivot;
-        }
-        if (target >= nums[0]) {
-            return binarySearch(nums, target, 0, pivot - 1);
-        }
-        return binarySearch(nums, target, pivot + 1, nums.length - 1);
-    }
-    
-    int binarySearch(int[] arr, int target, int start, int end) {
-        while(start <= end) {
-            // find the middle element 
-            // int mid = (start + end) / 2;  // might be possible that start + end exceeds the range of int in java
-            int mid = start + (end - start) / 2;
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int lo = 0, hi = n - 1;
 
-            if(target < arr[mid]) {
-                end = mid - 1;
-            } else if(target > arr[mid]) {
-                start = mid + 1;
-            } else {
-                return mid; // element found at mid index 
-            }
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            
+            if (nums[mid] > nums[n - 1]) lo = mid + 1;
+            else hi = mid;
         }
-        return -1; // element not found.
-    }
 
-    int findPivot(int[] arr) {
-        int start = 0;
-        int end = arr.length - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (mid < end && arr[mid] > arr[mid + 1]) {
-                return mid;
-            }
-            if (mid > start && arr[mid] < arr[mid - 1]) {
-                return mid - 1;
-            }
-            if (arr[start] >= arr[mid]) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
+        int rot = lo;
+        lo = 0; hi = n - 1;
+
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            int real = (mid + rot) % n;
+
+            if (nums[real] == target)
+                return real;
+            
+            if (nums[real] < target) lo = mid + 1;
+            else hi = mid - 1;
         }
         return -1;
     }
