c814ea7cb682848cfb4c8682d152cb5dd192b8fc
Diptansh <diptanshnagar@gmail.com>
2026-07-27 22:00:55 +0530

    Time: 7 ms (34.97%), Space: 45.5 MB (29.6%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,26 +1,40 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> st = new Stack<>();
-        String operators = "+-/*";
+        Stack<Integer> stk = new Stack<>();
 
-        for(String token : tokens) {
-            if(operators.indexOf(token) != -1) {
-                int d2 = st.pop();
-                int d1 = st.pop();
+        for (String token : tokens) {
 
-                if(token.equals("+")) {
-                    st.push(d2 + d1);
+            // Check if the token is an operator
+            if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
+
+                // Pop right operand first
+                int d2 = stk.pop();
+
+                // Pop left operand second
+                int d1 = stk.pop();
+
+                int res = 0;
+
+                if (token.equals("+")) {
+                    res = d1 + d2;
                 } else if (token.equals("-")) {
-                    st.push(d1 - d2);
-                } else if (token.equals("/")) {
-                    st.push(d1 / d2);
+                    res = d1 - d2;
                 } else if (token.equals("*")) {
-                    st.push(d2 * d1);
+                    res = d1 * d2;
+                } else {
+                    res = d1 / d2;
                 }
+
+                // Push the result back onto the stack
+                stk.push(res);
+
             } else {
-                st.push(Integer.parseInt(token));
-            }    
+                // Token is a number, convert it from String to int and push it
+                stk.push(Integer.parseInt(token));
+            }
         }
-        return st.peek();
+
+        // Final result is the only element left in the stack
+        return stk.pop();
     }
 }
