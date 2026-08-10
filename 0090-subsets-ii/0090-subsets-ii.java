1b30242d8e224de12a3238f5d4e324a58a1525d8
Deeptansh Nagar <diptanshnagar@gmail.com>
2026-08-10 15:08:11 +0530

    Time: 2 ms (99.72%), Space: 45 MB (77.28%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,22 +1,30 @@
 class Solution {
-    public List<List<Integer>> subsetsWithDup(int[] arr) {
-        Arrays.sort(arr);
-        List<List<Integer>> outer = new ArrayList<>();
-        outer.add(new ArrayList<>());
-        int start = 0;
-        int end = 0;
-        for (int i = 0; i < arr.length; i++) { 
-            start = 0;
-            if (i > 0 && arr[i] == arr[i - 1]) {
-                start = end + 1;
-            }
-            end = outer.size() - 1;
-            for (int j = start; j <= end; j++) {
-                List<Integer> internal = new ArrayList<>(outer.get(j));
-                internal.add(arr[i]);
-                outer.add(internal);
-            }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> resultant = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        int i = 0;
+        resultantFunction(nums, i, ans, resultant);
+        return resultant;
+    }
+
+    public List<List<Integer>> resultantFunction(int[] nums, int i, List<Integer> ans, List<List<Integer>> resultant) {
+        if(i == nums.length) {
+            resultant.add(new ArrayList<>(ans));
+            return resultant;
         }
-        return outer;
+
+        ans.add(nums[i]);
+        resultantFunction(nums, i + 1, ans, resultant);
+
+        ans.remove(ans.size() - 1);
+        while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
+            i++;
+        }
+
+
+        resultantFunction(nums, i + 1, ans, resultant);
+
+        return resultant;
     }
 }
